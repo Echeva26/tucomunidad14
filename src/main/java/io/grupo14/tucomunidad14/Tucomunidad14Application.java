eadb700f1f6e3c434f904132cb37b1959f1c23a6
Echeva26 <jeu2604@gmail.com>
2024-04-02 19:51:20 +0200

    Versión inicial del proyecto vacío

---
+++ src/main/java/io/grupo14/tucomunidad14/Tucomunidad14Application.java
@@ -0,0 +1,13 @@
+package io.grupo14.tucomunidad14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tucomunidad14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tucomunidad14Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/grupo14/tucomunidad14/Tucomunidad14ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.grupo14.tucomunidad14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tucomunidad14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
