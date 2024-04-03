8c2d863e4126c93c3c81a8799026fdfb58805a38
Echeva26 <jeu2604@gmail.com>
2024-04-03 12:23:45 +0200

    prueba

--- src/main/java/io/grupo14/tucomunidad14/Tucomunidad14Application.java
+++ src/main/java/io/grupo14/tucomunidad14/Tucomunidad14Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Tucomunidad14Application {
-//Carmen 
+//jorge
 	public static void main(String[] args) {
 		SpringApplication.run(Tucomunidad14Application.class, args);
 	}
