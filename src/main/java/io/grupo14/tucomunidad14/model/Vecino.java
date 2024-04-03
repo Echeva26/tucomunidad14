02063ee556734b15ad1dc632e61c7011835f8a78
Echeva26 <jeu2604@gmail.com>
2024-04-03 17:35:53 +0200

    Clase vecino

---
+++ src/main/java/io/grupo14/tucomunidad14/model/Vecino.java
@@ -0,0 +1,69 @@
+package io.grupo14.tucomunidad14.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(name= "users")
+public class Vecino {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    private String Apellidos;
+    private @Email String tutor;
+    private Integer portal;
+    private Integer piso;
+    private Integer telefono;
+
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return Apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+    public String getTutor() {
+        return tutor;
+    }
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+    public Integer getPortal() {
+        return portal;
+    }
+    public void setPortal(Integer portal) {
+        this.portal = portal;
+    }
+    public Integer getPiso() {
+        return piso;
+    }
+    public void setPiso(Integer piso) {
+        this.piso = piso;
+    }
+    public Integer getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+
+
+}
