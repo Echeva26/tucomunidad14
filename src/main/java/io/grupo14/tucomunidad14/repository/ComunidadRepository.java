894d309e8f1efb9c636304eddd1d1583724442d9
Echeva26 <jeu2604@gmail.com>
2024-04-04 13:52:34 +0200

    Nuevo Repositorio y cambios

--- src/main/java/io/grupo14/tucomunidad14/repository/ComunidadRepository.java
+++ src/main/java/io/grupo14/tucomunidad14/repository/ComunidadRepository.java
@@ -2,12 +2,13 @@ package io.grupo14.tucomunidad14.repository;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import io.grupo14.tucomunidad14.model.Comunidad;
 import io.grupo14.tucomunidad14.model.Vecino;
 
 
-
+@Repository
 public interface ComunidadRepository extends CrudRepository<Comunidad,Long> {
     Comunidad findByVecinoContains(Vecino vecino);
 
--- src/main/java/io/grupo14/tucomunidad14/repository/ReservasRepository.java
+++ src/main/java/io/grupo14/tucomunidad14/repository/ReservasRepository.java
@@ -1,13 +1,12 @@
 package io.grupo14.tucomunidad14.repository;
 
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import io.grupo14.tucomunidad14.model.Areacomun;
+import io.grupo14.tucomunidad14.model.Reserva;
 
 
 @Repository
-public interface AreacomunRepository extends CrudRepository<Areacomun,Long> {
+public interface ReservasRepository extends CrudRepository<Reserva,Long> {
 
 }
