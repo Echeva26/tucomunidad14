8ed55fe6ef7ef8878673801198e83bdf15292ba6
cvperez27 <c.vperezdelacruz@alumnos.upm.com>
2024-04-04 13:01:32 +0200

    Hola

---
+++ src/main/java/io/grupo14/tucomunidad14/controller/Reservas.java
@@ -0,0 +1,29 @@
+package io.grupo14.tucomunidad14.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+public class Reservas {
+    @GetMapping("/reservas")
+    
+    public String reservas() {
+        return "reservas" ;
+    }
+    
+    @GetMapping("/reservas/pistaspadel/{comunidad}")
+    public String getPistasdePadel(@RequestParam String id) {
+        return new String();
+    }
+
+    @GetMapping("/reservas/local/{comunidad}")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    
+
+}
--- src/main/java/io/grupo14/tucomunidad14/repository/ComunidadRepository.java
+++ src/main/java/io/grupo14/tucomunidad14/repository/ComunidadRepository.java
@@ -4,8 +4,11 @@ package io.grupo14.tucomunidad14.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import io.grupo14.tucomunidad14.model.Comunidad;
+import io.grupo14.tucomunidad14.model.Vecino;
+
+
 
 public interface ComunidadRepository extends CrudRepository<Comunidad,Long> {
-
+    Comunidad findByVecinoContains(Vecino vecino);
 
 }
