98ce5b9a9d93af5756e2d2cbe56c50fa3c0664aa
Echeva26 <jeu2604@gmail.com>
2024-04-06 19:44:14 +0200

    AREACREADA

--- src/main/java/io/grupo14/tucomunidad14/controller/Areacontroller.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/Areacontroller.java
@@ -3,29 +3,39 @@ package io.grupo14.tucomunidad14.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.grupo14.tucomunidad14.model.Areacomun;
-import io.grupo14.tucomunidad14.model.Tipodearea;
+import io.grupo14.tucomunidad14.model.AreacomunDTO;
+import io.grupo14.tucomunidad14.model.Comunidad;
 import io.grupo14.tucomunidad14.repository.AreacomunRepository;
-
+import io.grupo14.tucomunidad14.repository.ComunidadRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import org.springframework.web.bind.annotation.RequestParam;
 
 //hola
 @RestController
 public class Areacontroller {
     @Autowired
     private AreacomunRepository areacomunRepository;
-    @PostMapping("/creararea")
-    public String CrearArea (@RequestParam String nombre, @RequestParam Tipodearea tipodearea, @RequestParam Long idComunidad){
-        Areacomun nuevaArea = new Areacomun();
-        nuevaArea.setNombre(nombre);
-        nuevaArea.setTipodearea(tipodearea);
-        areacomunRepository.save(nuevaArea);
-        return "Area creada exitosamente";
-    }
 
+    @Autowired 
+    private ComunidadRepository comunidadRepository;
+    @PostMapping("/creararea")
+
+
+    public String crearArea(@RequestBody AreacomunDTO areacomunDTO) {
+    Comunidad comunidad = comunidadRepository.findById(areacomunDTO.getIdComunidad()).orElseThrow(
+        () -> new RuntimeException("Comunidad no encontrada con id: " + areacomunDTO.getIdComunidad())
+    );
+
+    Areacomun nuevaArea = new Areacomun();
+    nuevaArea.setNombre(areacomunDTO.getNombre());
+    nuevaArea.setTipodearea(areacomunDTO.getTipodearea());
+    nuevaArea.setComunidad(comunidad); // Asignar la comunidad encontrada
+    areacomunRepository.save(nuevaArea);
+    return "Se ha creado correctamente el area";
+}
 }
