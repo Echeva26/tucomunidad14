d53577171b882f9918568e088fa8f25294df3d35
cvperez27 <165889653+cvperez27@users.noreply.github.com>
2024-04-06 19:30:21 +0200

    errata

--- src/main/java/io/grupo14/tucomunidad14/controller/Areacontroller.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/Areacontroller.java
@@ -24,7 +24,6 @@ public class Areacontroller {
         Areacomun nuevaArea = new Areacomun();
         nuevaArea.setNombre(nombre);
         nuevaArea.setTipodearea(tipodearea);
-        nuevaArea.setComunidad(idComunidad); 
         areacomunRepository.save(nuevaArea);
         return "Area creada exitosamente";
     }
