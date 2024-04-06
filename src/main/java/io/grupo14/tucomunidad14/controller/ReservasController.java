3a1f06947525c63970ea89e8530a16f893b13549
Echeva26 <jeu2604@gmail.com>
2024-04-06 16:46:28 +0200

    Delete reserva

--- src/main/java/io/grupo14/tucomunidad14/controller/ReservasController.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/ReservasController.java
@@ -17,7 +17,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -69,28 +71,34 @@ public class ReservasController {
 
     @PostMapping("/reservas")
     public ResponseEntity<Reserva> crearReserva(@RequestBody ReservaSimpleDTO reservaDTO) {
-        // Paso 1: Buscar las entidades relacionadas basadas en el DTO
+        
         Areacomun areaComun = areaComunRepository.findById(reservaDTO.getIdarea()).orElseThrow(() -> new RuntimeException("Área común no encontrada"));
         Vecino vecino = vecinoRepository.findById(reservaDTO.getIdvecino()).orElseThrow(() -> new RuntimeException("Vecino no encontrado"));
 
-        // Aquí ya tenemos el área común, que incluye el tipo de área como un parámetro enumerable
-
-        // Paso 2: Crear la entidad Reserva con los datos y relaciones
+        
         Reserva nuevaReserva = new Reserva();
         nuevaReserva.setAreacomun(areaComun);
         nuevaReserva.setVecino(vecino);
         nuevaReserva.setInicioReserva(reservaDTO.getInicioReserva());
         nuevaReserva.setFinReserva(reservaDTO.getFinReserva());
-        // Establecer cualquier otro campo necesario de Reserva desde reservaDTO
-
-        // Paso 3: Guardar la reserva en la base de datos
+       
         Reserva reservaGuardada = reservasRepository.save(nuevaReserva);
 
-        // Paso 4: Devolver la reserva guardada y un estado HTTP adecuado
+        
         return ResponseEntity.ok(reservaGuardada);
     }
 
-    //Acuerdate que tienes que sacar por tipo para las pantallas 
+    @DeleteMapping("/reservas/{id}")
+    public ResponseEntity<Reserva> eliminarReserva(@PathVariable Long id) {
+        if (!reservasRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        reservasRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    
 }
     
 
