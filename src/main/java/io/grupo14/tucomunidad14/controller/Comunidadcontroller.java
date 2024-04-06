7d5f23ca95e3f4cca4d33b102057a839ee306aab
Echeva26 <jeu2604@gmail.com>
2024-04-06 17:35:09 +0200

    cmabios tontos

--- src/main/java/io/grupo14/tucomunidad14/controller/Comunidadcontroller.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/Comunidadcontroller.java
@@ -2,22 +2,26 @@ package io.grupo14.tucomunidad14.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.grupo14.tucomunidad14.model.Comunidad;
 
-import io.grupo14.tucomunidad14.model.Reserva;
-import io.grupo14.tucomunidad14.model.ReservaSimpleDTO;
-import io.grupo14.tucomunidad14.repository.ReservasRepository;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.grupo14.tucomunidad14.repository.ComunidadRepository;
+
+
+
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
-public class ComunidadController {
+public class Comunidadcontroller {
     @Autowired
     private ComunidadRepository comunidadRepository;
 
@@ -28,8 +32,7 @@ public class ComunidadController {
         Comunidad nuevaComunidad = new Comunidad();
         nuevaComunidad.setNombre(nombre);
         nuevaComunidad.setCodpostal(codpostal);
-        nuevaComunidad.setVecinos(new ArrayList<>()); // Lista de vecinos inicialmente vacía
-        nuevaComunidad.setArea(new ArrayList<>()); // Lista de áreas comunes inicialmente vacía
+         // Lista de áreas comunes inicialmente vacía
 
         comunidadRepository.save(nuevaComunidad);
 
@@ -37,19 +40,7 @@ public class ComunidadController {
     }
 
 //Cuando pulso el botón unirse
-    @PostMapping("/comunidad/unirse")
-    @ResponseBody
-    public String agregarVecinoAComunidad(@PathVariable Long comunidadId, @RequestBody Vecino nuevoVecino) {
-        Comunidad comunidad = comunidadRepository.findById(comunidadId).orElse(null);
-        if (comunidad != null) {
-            comunidad.getVecinos().add(nuevoVecino);
-            nuevoVecino.setComunidad(comunidad); // Asignamos la comunidad al vecino
-            comunidadRepository.save(comunidad);
-            return "Vecino añadido exitosamente a la comunidad con ID " + comunidadId;
-        } else {
-            return "No se encontró la comunidad con ID " + comunidadId;
-        }
-    }
+    
 
 
 
