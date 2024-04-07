19056598001cdde391e6e73266839719410ae722
cvperez27 <165889653+cvperez27@users.noreply.github.com>
2024-04-07 12:41:40 +0200

    Comunidad

--- src/main/java/io/grupo14/tucomunidad14/controller/Comunidadcontroller.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/Comunidadcontroller.java
@@ -21,14 +21,36 @@ public class Comunidadcontroller {
     private ComunidadRepository comunidadRepository;
 
 //Cuando hago un post para crear una comunidad
-    @PostMapping("/crearcomunidad")
-    @ResponseBody
-    public String crearComunidad(@RequestBody ComunidadDTO comunidadDTO) {
+   @PostMapping("/crearcomunidad")
+   @ResponseBody
+    public String crearComunidad(@RequestParam ComunidadDTO comunidadDTO) {
         Comunidad nuevaComunidad = new Comunidad();
         nuevaComunidad.setNombre(comunidadDTO.getNombre());
         nuevaComunidad.setCodpostal(comunidadDTO.getCodpostal());
         comunidadRepository.save(nuevaComunidad); 
         return "Comunidad creada exitosamente";
     }
+
+    @PostMapping("/crearcomunidad")
+    @ResponseBody
+    public String crearComunidad(@RequestBody ComunidadDTO comunidadDTO,
+                                 @RequestParam("zonas_comunes") List<String> zonasComunes) {
+        Comunidad nuevaComunidad = new Comunidad();
+        nuevaComunidad.setNombre(comunidadDTO.getNombre());
+        nuevaComunidad.setCodpostal(comunidadDTO.getCodpostal());
+        comunidadRepository.save(nuevaComunidad);
+
+        // Crear las áreas comunes
+        for (String zonaComun : zonasComunes) {
+            Areacomun nuevaArea = new Areacomun();
+            nuevaArea.setNombre(zonaComun);
+            nuevaArea.setTipodearea(zonaComun);
+            nuevaArea.setComunidad(nuevaComunidad);
+            areacomunRepository.save(nuevaArea);
+        }
+
+        return "Comunidad creada exitosamente";
+    }
+}
 
 }
