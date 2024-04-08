84a1a46d49deacce00c8cfdb47cd939110fdccf2
Echeva26 <jeu2604@gmail.com>
2024-04-08 09:04:18 +0200

    oraginzacion

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/ReservaController.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/ReservaController.java
@@ -1,4 +1,4 @@
-package io.grupo14.tucomunidad14.controller;
+package io.grupo14.tucomunidad14.Controllerweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
