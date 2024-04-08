2545170db886c9e4bea89b546ed367e6b6ac578c
Echeva26 <jeu2604@gmail.com>
2024-04-08 10:02:42 +0200

    crearcomunidad

---
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/Crearcomunidad.java
@@ -0,0 +1,18 @@
+package io.grupo14.tucomunidad14.Controllerweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Crearcomunidad {
+
+
+   
+
+
+    @GetMapping("/crearcomunidad")
+    public String index() {
+        return "pag9act"; 
+    }    
+
+}
