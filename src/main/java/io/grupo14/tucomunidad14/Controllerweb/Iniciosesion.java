b3d1226def20ad1b2954e1a05a7c99b37b7e46b2
Echeva26 <jeu2604@gmail.com>
2024-04-08 09:56:28 +0200

    inicio sesion

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/Iniciosesion.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/Iniciosesion.java
@@ -1,7 +1,10 @@
 package io.grupo14.tucomunidad14.Controllerweb;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
 public class Iniciosesion {
     
 
