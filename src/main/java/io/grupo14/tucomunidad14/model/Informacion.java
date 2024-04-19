1ac5c32a75ae019ad96b064b667b7e1e71f9e7c3
Echeva26 <jeu2604@gmail.com>
2024-04-19 18:03:09 +0200

    Cambios base de datos SPRINT 3

---
+++ src/main/java/io/grupo14/tucomunidad14/model/Informacion.java
@@ -0,0 +1,98 @@
+package io.grupo14.tucomunidad14.model;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@EntityScan
+@Table(name= "INFORMACION")
+public class Informacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idinformacion;
+    private String titulo;
+    private Date fecha;
+    private Object foto;//Cambios?
+    private String descripcion;
+    private String textocompleto;//Cambios?
+    
+    @ManyToMany(mappedBy = "informacions")
+    @JoinColumn(name = "vecino")
+    private List<Vecino> vecino;
+    @ManyToOne
+    @JoinColumn(name = "comunidad")
+    private Comunidad comunidad;
+    // Campo para el inicio de la reserva
+    
+
+
+    public Long getIdinformacion() {
+        return idinformacion;
+    }
+    public Informacion() {
+    }
+    public void setIdinformacion(Long idinformacion) {
+        this.idinformacion = idinformacion;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    public Object getFoto() {
+        return foto;
+    }
+    public void setFoto(Object foto) {
+        this.foto = foto;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getTextocompleto() {
+        return textocompleto;
+    }
+    public void setTextocompleto(String textocompleto) {
+        this.textocompleto = textocompleto;
+    }
+    public List<Vecino> getVecino() {
+        return vecino;
+    }
+    public void setVecino(List<Vecino> vecino) {
+        this.vecino = vecino;
+    }
+    public Comunidad getComunidad() {
+        return comunidad;
+    }
+    public void setComunidad(Comunidad comunidad) {
+        this.comunidad = comunidad;
+    }
+    
+    
+    
+
+    
+}
+
--- src/main/java/io/grupo14/tucomunidad14/model/Vecino.java
+++ src/main/java/io/grupo14/tucomunidad14/model/Vecino.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -30,7 +32,12 @@ public class Vecino {
     private Comunidad comunidad;
     @OneToMany(mappedBy = "vecino")
     private List<Reserva> reserva;
-
+    @ManyToMany
+    @JoinTable(
+        name= "vecino-informacion", joinColumns = @JoinColumn(name = "idvecino",referencedColumnName = "idvecino"),
+        inverseJoinColumns = @JoinColumn(name = "idinformacion",referencedColumnName = "idinformacion")
+    )
+    private List<Informacion> informacions;
 
     public Vecino() {
        
