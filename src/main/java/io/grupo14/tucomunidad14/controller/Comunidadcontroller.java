d3c0d1594969e2cc20e2ff7d78c344094e342783
Echeva26 <jeu2604@gmail.com>
2024-04-21 13:12:05 +0200

    Cambios

--- src/main/java/io/grupo14/tucomunidad14/controller/Comunidadcontroller.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/Comunidadcontroller.java
@@ -6,22 +6,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.grupo14.tucomunidad14.model.Areacomun;
+
 import io.grupo14.tucomunidad14.model.Comunidad;
 import io.grupo14.tucomunidad14.model.ComunidadDTO;
-import io.grupo14.tucomunidad14.model.Tipodearea;
-import io.grupo14.tucomunidad14.repository.AreacomunRepository;
+
 import io.grupo14.tucomunidad14.repository.ComunidadRepository;
 
 @RestController
 public class Comunidadcontroller {
-    private static final Tipodearea LOCAL = null;
+    
 
     @Autowired
     private ComunidadRepository comunidadRepository;
 
-    @Autowired
-    private AreacomunRepository areacomunRepository;
+  
     @PostMapping("/crearcomunidad")
     @ResponseBody
     public String crearComunidad(@RequestBody ComunidadDTO comunidadDTO) {
@@ -30,15 +28,8 @@ public class Comunidadcontroller {
         nuevaComunidad.setCodpostal(comunidadDTO.getCodpostal());
         comunidadRepository.save(nuevaComunidad);
 
-        Areacomun areacomun = new Areacomun();
-        Tipodearea Local = LOCAL;
-        areacomun.setTipodearea(Local);
-        areacomun.setNombre("Local");
-        areacomun.setComunidad(nuevaComunidad);
-        areacomunRepository.save(areacomun);
-
-
-        return "Comunidad creada exitosamente";
+    
+        return "Comunidad creada exitosamente con id :"+nuevaComunidad.getIdcomunidad();
     }
 
 }
