62fa402969cdd75099db366d432a233370be4ee3
cvperez27 <c.vperezdelacruz@alumnos.upm.com>
2024-04-21 16:10:51 +0200

    Revert "nADA"
    This reverts commit 2ee5b7e188d869ba1b79611247e70aeb08cadbce, reversing
    changes made to 93dc625e60af788aa6585d3803378d57f5d72ae9.

--- src/main/java/io/grupo14/tucomunidad14/model/AreacomunDTO.java
+++ src/main/java/io/grupo14/tucomunidad14/model/AreacomunDTO.java
@@ -1,35 +1,28 @@
-
 package io.grupo14.tucomunidad14.model;
 
 public class AreacomunDTO {
     private String nombre;
-    private int tipodearea; // Cambio aquí de Tipodearea a int
+    private String tipodearea;
     private Long idComunidad; // Utilizar este campo para recibir el ID de la comunidad
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    // Cambio el getter y setter para manejar un int
-    public int getTipodearea() {
+    public String getTipodearea() {
         return tipodearea;
     }
-
-    public void setTipodearea(int tipodearea) {
+    public void setTipodearea(String tipodearea) {
         this.tipodearea = tipodearea;
     }
-
     public Long getIdComunidad() {
         return idComunidad;
     }
-
     public void setIdComunidad(Long idComunidad) {
         this.idComunidad = idComunidad;
     }
-}
 
+   
+}
 
--- src/main/java/io/grupo14/tucomunidad14/model/Tipodearea.java
+++ src/main/java/io/grupo14/tucomunidad14/model/Tipodearea.java
@@ -1,28 +1,9 @@
 package io.grupo14.tucomunidad14.model;
 
 public enum Tipodearea {
-    PADDEL(0, "Pistas de pádel"),
-    LOCAL(1, "Local"),
-    GIMNASIO(2, "Gimnasio");
 
-    private final int value;
-    private final String description;
-
-    Tipodearea(int value, String description) {
-        this.value = value;
-        this.description = description;
-    }
-
-    public static Tipodearea fromValue(int value) {
-        for (Tipodearea type : values()) {
-            if (type.value == value) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Tipo de área no reconocido: " + value);
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    PISTADEPADEL,
+    LOCAL,
+    GIMNASIO,
+ 
 }
