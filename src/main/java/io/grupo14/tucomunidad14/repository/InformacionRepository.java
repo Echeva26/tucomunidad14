dea8578279786797be78c5261b0e0dbee6b716fb
Echeva26 <jeu2604@gmail.com>
2024-04-22 13:58:37 +0200

    Poco a poco

--- src/main/java/io/grupo14/tucomunidad14/repository/InformacionRepository.java
+++ src/main/java/io/grupo14/tucomunidad14/repository/InformacionRepository.java
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import io.grupo14.tucomunidad14.model.Informacion;
-import io.grupo14.tucomunidad14.model.InformacionDTO;
+
 
 @Repository
 public interface InformacionRepository extends CrudRepository<Informacion, Long> {
     //SELECT c FROM Comunidad c JOIN c.vecinos v WHERE v.idvecino = :idVecino
-    @Query("SELECT e FROM INFORMACION e JOIN e.comunidads WHERE e.idcomunidad = :idcomunidad")
-    List<Informacion> findByComunidadId(@Param("idcomunidad") Long idcomunidad);
+    @Query("SELECT i FROM Informacion i WHERE i.comunidad.id = :idcomunidad")
+List<Informacion> findByComunidadId(@Param("idcomunidad") Long idcomunidad);
 
 }
