4b06ddd0efbfd9f9ac28a3e6c5007d7ff9b7ff4c
mmooppss <agruizesc@gmail.com>
2024-04-24 22:20:52 +0200

    borrado archivos innecesarios

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/ReservaHorarioController.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/ReservaHorarioController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ReservaGimnasioController {
+public class ReservaHorarioController {
 
-    @GetMapping("/reserva-gimnasio")
+    @GetMapping("/reserva-horario")
     public String reservaTest() {
-        return "reserva-gimnasio"; 
+        return "reserva-horario"; 
     }
 }
