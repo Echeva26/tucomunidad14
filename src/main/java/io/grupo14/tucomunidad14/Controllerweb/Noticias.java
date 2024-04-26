723c936997b9ebf080c7a4d89846319355fe3e89
ginebra <g.yun@alumnos.upm.es>
2024-04-26 12:08:03 +0200

    Funciona boton noticia

---
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/Noticias.java
@@ -0,0 +1,12 @@
+package io.grupo14.tucomunidad14.Controllerweb;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Noticias {
+    
+    @GetMapping("/noticias")
+    public String noticias() {
+        return "noticias"; 
+    }   
+}
