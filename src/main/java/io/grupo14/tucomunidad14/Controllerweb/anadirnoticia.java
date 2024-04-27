2e6ad4e26dbc48aa5a948502172fc7539eaf8888
Echeva26 <jeu2604@gmail.com>
2024-04-27 19:12:44 +0200

    tonterias

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/anadirnoticia.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/anadirnoticia.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class anadirnoticia {
 
     @GetMapping("/anadirnoticia")
-    public String anadirnoticia() {
+    public String noesuncontructor() {
         return "añadirnoticiaadmin"; 
     }  
 }
