c1b7994f43a5a73a9837742993e5d98778620b15
ginebra <g.yun@alumnos.upm.es>
2024-04-27 18:25:41 +0200

    controller añadir noticia definitivo

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/anadirnoticia.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/anadirnoticia.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class añadirnoticia {
+public class anadirnoticia {
 
     @GetMapping("/anadirnoticia")
     public String anadirnoticia() {
-        return "anadirnoticia"; 
+        return "añadirnoticiaadmin"; 
     }  
 }
