39a925a069387535025e9c1909a9e9511ce5bbf0
mmooppss <agruizesc@gmail.com>
2024-05-03 14:19:50 +0200

    vernoticia controller

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/vernoticia.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/vernoticia.java
@@ -1,11 +1,14 @@
 package io.grupo14.tucomunidad14.Controllerweb;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class vernoticia {
-    @GetMapping("/vernoticia")
-    public String vernoticias() {
-        return "vernoticia";
-    }
+@Controller
+public class Vernoticia {
+    
 
+    @GetMapping("/vernoticia")
+    public String menucomunidad() {
+        return "vernoticia"; 
+    }
 }
