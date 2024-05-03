6c07bbe4881fd2d874d6a6cdc8cfd144bafe93a1
cvperez27 <c.vperezdelacruz@alumnos.upm.com>
2024-05-03 14:02:32 +0200

    Añadir y ver noticias

--- src/main/java/io/grupo14/tucomunidad14/Controllerweb/anadirnoticia.java
+++ src/main/java/io/grupo14/tucomunidad14/Controllerweb/anadirnoticia.java
@@ -8,6 +8,6 @@ public class anadirnoticia {
 
     @GetMapping("/anadirnoticia")
     public String noticia() {
-        return "añadirnoticiaadmin"; 
-    }  
+        return "añadirnoticiaadmin";
+    }
 }
