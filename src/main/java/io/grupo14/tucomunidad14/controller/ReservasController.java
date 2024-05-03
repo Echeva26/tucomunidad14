178d0fa57a25b8421ec1b495185b9d8a1136f513
Echeva26 <jeu2604@gmail.com>
2024-05-03 11:52:20 +0200

    Tonterias

--- src/main/java/io/grupo14/tucomunidad14/controller/ReservasController.java
+++ src/main/java/io/grupo14/tucomunidad14/controller/ReservasController.java
@@ -32,8 +32,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 public class ReservasController {
